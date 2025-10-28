a5f1befe43d46521f497149d9bd6ee9d99d5ca54
Dmytro <dku4it@gmail.com>
2025-10-28 16:46:16 +0200

    Add files via upload

---
+++ Randomizer/Randomizer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Randomizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Гра "Вгадай число"
+            Програма випадковим чином генерує число від 1 до 1000.
+            Користувач має 7 спроб, щоб відгадати це число.
+            Після кожної невдалої спроби програма повинна повідомляти, чи є введене число меншим або більшим за задумане.
+            Якщо користувач вгадує - вивести повідомлення про перемогу.
+            Якщо не вгадує за 7 спроб - повідомити, що гру програно. */
+
+
+            int userNumber1;
+         
+            Random random1 = new Random((int)DateTime.Now.Ticks);
+            int random2 = random1.Next(1, 1001);
+
+            Console.Write("Try to guess the number. You have 7 attempts.\nEnter your number (from 1 to 1000): ");
+          
+
+            for (int counter = 7; counter >= 0 ; counter--)
+            {
+
+
+                if (counter == 0)
+                {
+                    Console.WriteLine("You are lose. It was " + random2);
+                    break;
+                }
+                else 
+                {
+                    userNumber1 = int.Parse(Console.ReadLine());
+
+
+                    if (random2 == userNumber1)
+                    {
+                    Console.WriteLine("\nYou're win!!! It was really " + random2 + "!!!");
+                    break;
+                    }
+                    else
+                    {
+                        if (random2 < userNumber1)
+                        {
+                        Console.WriteLine("Too high!\n");
+                        }
+                        else
+                        {
+                        Console.WriteLine("Too low!\n");
+                        }
+
+                    }
+
+                    Console.WriteLine("\nYou have " + (counter - 1) + " attempt");
+
+                }
+
+            }
+           
+        }
+    }
+}
